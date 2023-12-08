496e73680c5ddcfcdb9875cb21b310a42d233679
Vladimir <kimvovik2000@mail.ru>
2023-12-08 14:09:12 +1000

    Make modal and class for menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -85,6 +85,88 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   ;
   setClock('.timer', deadline);
+
+  //Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = modal.querySelector('.modal__close');
+  modalTrigger.forEach(item => {
+    item.addEventListener('click', openModal);
+  });
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+  }
+  ;
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+  ;
+  modalCloseBtn.addEventListener('click', closeModal);
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  // const modalTimeId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+  ;
+  window.addEventListener('scroll', showModalByScroll);
+
+  //Использую классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 2.52;
+      this.changeToRub();
+    }
+    changeToRub() {
+      this.price = this.price * this.transfer;
+      this.price = Math.floor(this.price);
+    }
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+                <div class="menu__item">
+                    <img src='${this.src}' alt='${this.alt}'>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span></span> руб/день</div>
+                    </div>
+                </div>
+            `;
+      this.parent.append(element);
+    }
+  }
+  ;
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229, '.menu .container').render();
+  new MenuCard("img/tabs/elite.jpg", "elite", 'Меню "Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550, '.menu .container').render();
+  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 430, '.menu .container').render();
 });
 /******/ })()
 ;
